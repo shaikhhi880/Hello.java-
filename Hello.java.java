8e0351b088fb4c4f301850cae30a6a5d8c4f7846
shaikhhi880 <54373778+shaikhhi880@users.noreply.github.com>
2019-08-21 14:29:35 -0400

    Add files via upload

---
+++ Hello.java.java
@@ -0,0 +1,23 @@
+/*****************************************************************
+*Java Program Name 
+*****************************************************************
+* Project/Program Description
+* Humza Shaikh 	
+* 08212019
+* CS 255 02
+*****************************************************************/
+public class Hello {
+ public static void printHeading() {
+	System.out.println("Humza Shaikh");
+	System.out.println("Java Project");
+	System.out.println("The goal of this lab is to download Java");
+	System.out.println("Version date");
+	System.out.println("CS 255 02");
+	System.out.println();
+}
+
+ public static void main(String[]rgs) {
+  printHeading();
+  System.out.println();
+  }
+ }
